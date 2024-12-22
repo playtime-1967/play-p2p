04aeee7493068623d82c15eb03323ce8d1f63576
Saman Namnik <saman.namnik@gmail.com>
2024-12-22 13:24:39 +0000

    implement a basic distributed key-value store using libp2p, along with the mDNS and Kademlia protocols

--- src/chat.rs
+++ src/chat.rs
@@ -25,9 +25,9 @@ struct MyBehaviour {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let _ = tracing_subscriber::fmt()
+    tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
-        .try_init();
+        .init();
 
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
---
+++ src/distributed-key-value-store.rs
@@ -0,0 +1,221 @@
+use std::error::Error;
+
+use futures::stream::StreamExt;
+use libp2p::{
+    kad,
+    kad::{store::MemoryStore, Mode},
+    mdns, noise,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux,
+};
+use tokio::{
+    io::{self, AsyncBufReadExt},
+    select,
+    time::Duration,
+};
+use tracing_subscriber::EnvFilter;
+
+// We create a custom network behaviour that combines Kademlia and mDNS.
+#[derive(NetworkBehaviour)]
+struct MyBehaviour {
+    kademlia: kad::Behaviour<MemoryStore>,
+    mdns: mdns::tokio::Behaviour,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity() //Creates a new peer identity with a cryptographic key pair for secure communication.
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|key| {
+            //Combining mDNS and Kademlia allows your node to function both locally and globally:
+            //mDNS helps bootstrap connections in small, local networks.
+            //Kademlia handles long-term peer discovery and routing in a larger network.
+            Ok(MyBehaviour {
+                kademlia: kad::Behaviour::new(
+                    key.public().to_peer_id(),
+                    MemoryStore::new(key.public().to_peer_id()),
+                ),
+                mdns: mdns::tokio::Behaviour::new(
+                    mdns::Config::default(),
+                    key.public().to_peer_id(),
+                )?,
+            })
+        })?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) //keep connections open indefinitely (u64::MAX) when idle
+        .build();
+
+    //Mode::Server, means that the node will both: 1- Accept and respond to incoming requests (e.g., GET or PUT).
+    //2- Participate in routing by storing data and forwarding requests to other peers in the network.
+    //Whereas Mode::Client does only the first item.
+    swarm.behaviour_mut().kademlia.set_mode(Some(Mode::Server));
+
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    // Listen on all interfaces and whatever port the OS assigns.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Kick it off.
+    loop {
+        select! {
+        Ok(Some(line)) = stdin.next_line() => {
+            handle_input_line(&mut swarm.behaviour_mut().kademlia, line);
+        }
+        event = swarm.select_next_some() => match event {
+            SwarmEvent::NewListenAddr { address, .. } => {
+                println!("Listening in {address:?}");
+            },
+            SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                for (peer_id, multiaddr) in list {
+                    println!("mDNS discovered a new peer: {peer_id} {multiaddr}");
+                    //In order for a node to join the DHT... When a remote peer initiates a connection and that peer is not yet in the routing table, the Kademlia
+                    //behaviour must be informed of an address on which that peer is listening for connections before it can be added to the routing table from where
+                    //it can subsequently be discovered by all peers in the DHT.
+                    swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr);
+                }
+            }
+            SwarmEvent::Behaviour(MyBehaviourEvent::Kademlia(kad::Event::OutboundQueryProgressed { result, ..})) => {
+                match result {
+                    kad::QueryResult::GetProviders(Ok(kad::GetProvidersOk::FoundProviders { key, providers, .. })) => {
+                        for peer in providers {
+                            println!(
+                                "Peer {peer:?} provides key {:?}",
+                                std::str::from_utf8(key.as_ref()).unwrap()
+                            );
+                        }
+                    }
+                    kad::QueryResult::GetProviders(Err(err)) => {
+                        eprintln!("Failed to get providers: {err:?}");
+                    }
+                    kad::QueryResult::GetRecord(Ok(
+                        kad::GetRecordOk::FoundRecord(kad::PeerRecord {
+                            record: kad::Record { key, value, .. },
+                            ..
+                        })
+                    )) => {
+                        println!(
+                            "Got record {:?} {:?}",
+                            std::str::from_utf8(key.as_ref()).unwrap(),
+                            std::str::from_utf8(&value).unwrap(),
+                        );
+                    }
+                    kad::QueryResult::GetRecord(Ok(_)) => {}
+                    kad::QueryResult::GetRecord(Err(err)) => {
+                        eprintln!("Failed to get record: {err:?}");
+                    }
+                    kad::QueryResult::PutRecord(Ok(kad::PutRecordOk { key })) => {
+                        println!(
+                            "Successfully put record {:?}",
+                            std::str::from_utf8(key.as_ref()).unwrap()
+                        );
+                    }
+                    kad::QueryResult::PutRecord(Err(err)) => {
+                        eprintln!("Failed to put record: {err:?}");
+                    }
+                    kad::QueryResult::StartProviding(Ok(kad::AddProviderOk { key })) => {
+                        println!(
+                            "Successfully put provider record {:?}",
+                            std::str::from_utf8(key.as_ref()).unwrap()
+                        );
+                    }
+                    kad::QueryResult::StartProviding(Err(err)) => {
+                        eprintln!("Failed to put provider record: {err:?}");
+                    }
+                    bootstrap_event => println!("{bootstrap_event:?}"),
+                }
+            }
+            connection_event => println!("{connection_event:?}"),
+        }
+        }
+    }
+}
+
+fn handle_input_line(kademlia: &mut kad::Behaviour<MemoryStore>, line: String) {
+    let mut args = line.split(' ');
+
+    match args.next() {
+        Some("GET") => {
+            let key = match args.next() {
+                Some(key) => kad::RecordKey::new(&key),
+                None => {
+                    eprintln!("Expected key");
+                    return;
+                }
+            };
+            kademlia.get_record(key);
+        }
+        Some("GET_PROVIDERS") => {
+            let key = {
+                match args.next() {
+                    Some(key) => kad::RecordKey::new(&key),
+                    None => {
+                        eprintln!("Expected key");
+                        return;
+                    }
+                }
+            };
+            kademlia.get_providers(key);
+        }
+        Some("PUT") => {
+            let key = {
+                match args.next() {
+                    Some(key) => kad::RecordKey::new(&key),
+                    None => {
+                        eprintln!("Expected key");
+                        return;
+                    }
+                }
+            };
+            let value = match args.next() {
+                Some(value) => value.as_bytes().to_vec(),
+                None => {
+                    eprintln!("Expected value");
+                    return;
+                }
+            };
+            //A record to store in the DHT.
+            let record = kad::Record {
+                key,
+                value,
+                publisher: None,
+                expires: None,
+            };
+            //Stores a record in the DHT, locally as well as at the nodes closest to the key as per the xor distance metric.
+            kademlia
+                .put_record(record, kad::Quorum::One)
+                .expect("Failed to store record locally.");
+        }
+        Some("PUT_PROVIDER") => {
+            let key = {
+                match args.next() {
+                    Some(key) => kad::RecordKey::new(&key),
+                    None => {
+                        eprintln!("Expected key");
+                        return;
+                    }
+                }
+            };
+            //publishes a provider record with the given key and identity of the local node to the peers closest to the key
+            kademlia
+                .start_providing(key)
+                .expect("Failed to start providing key");
+        }
+        _ => {
+            eprintln!("expected GET, GET_PROVIDERS, PUT or PUT_PROVIDER");
+        }
+    }
+}
+
+//How to run: Running two nodes
+//1- cargo run --bin diskeyval
+//wait for a few seconds
+//2- cargo run --bin diskeyval
